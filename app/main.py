b829a139bd555402762acc876d02fd5b234c09e3
ander11oc <ander11oc@users.noreply.github.com>
2026-06-18 12:57:52 -0500

    fix: allow vercel and localhost subdomains in CORS

--- app/main.py
+++ app/main.py
@@ -33,6 +33,7 @@ origins = [item.strip() for item in settings.CORS_ORIGINS.split(",") if item.str
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_origin_regex=r"https://.*\.vercel\.app|http://localhost(:\d+)?|https://localhost(:\d+)?",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
